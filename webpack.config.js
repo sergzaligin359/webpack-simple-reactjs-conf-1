4390cef226bb1ce9e514237dfff793e88e562769
sergzaligin359 <webdevjsphp@gmail.com>
2020-12-07 13:59:21 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-react',
+              ['@babel/env', { targets: { browsers: ['last 7 versions'] } }]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './public',
+    hot: true,
+    port: 3001,
+    open: true,
+    historyApiFallback: true
+  },
+  devtool: 'inline-source-map'
+}
